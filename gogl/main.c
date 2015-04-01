508c365f3814104db15cfda2764ad05c987367c0
YangDongchen [杨东晨] <yangdongchen@xiaomi.com>
2015-04-01 13:55:26 +0800

    Initial Commit

---
+++ gogl/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gogl
+//
+//  Created by eastany on 15/4/1.
+//  Copyright (c) 2015年 eastany. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
